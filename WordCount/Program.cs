01105e8a20e56da9f59a37d40b5fdb087173bd3e
atallison <tanner@atallison.com>
2023-06-13 15:14:42 -0500

    Creating a word count tool

---
+++ WordCount/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var option = args[0];
+var path = args[1];
+
+if (option == "-c")
+{
+    // Get the current path that the application is running in
+    var currentDirectory = Directory.GetCurrentDirectory();
+    // Append the path passed in to the current directory
+    var fullPath = Path.Combine(currentDirectory, path);
+
+    //Count the number of characters in the file
+    var text = File.ReadAllBytes(fullPath);
+    var numberOfCharacters = text.Length;
+    Console.WriteLine($"{numberOfCharacters} {path}");
+}
